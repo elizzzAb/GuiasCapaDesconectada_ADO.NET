55298f5b3e94459c0c935b16561868be501f1ecd
Elizabeth Abrego <an0174032023@unab.edu.sv>
2024-09-07 11:43:53 -0600

    Guía XIX - Proyecto 'CapaDesconectada', finalizada.

---
+++ CapaDesconectada/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaDesconectada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnObtenerNotipado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnObtenerNoTipado_Click_1(object sender, EventArgs e)
+        {
+            DataTable dataTable = new DataTable();
+
+            String select = "";
+            select = select + "SELECT [CustomerID] " + "\n";
+            select = select + "      ,[CompanyName] " + "\n";
+            select = select + "      ,[ContactName] " + "\n";
+            select = select + "      ,[ContactTitle] " + "\n";
+            select = select + "      ,[Address] " + "\n";
+            select = select + "      ,[City] " + "\n";
+            select = select + "      ,[Region] " + "\n";
+            select = select + "      ,[PostalCode] " + "\n";
+            select = select + "      ,[Country] " + "\n";
+            select = select + "      ,[Phone] " + "\n";
+            select = select + "      ,[Fax] " + "\n";
+            select = select + "  FROM [dbo].[Customers]";
+
+            //var conexion = @"Data Source=LAPTOP-GK1DJVHC\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;Trust Server Certificate=True"; //No me la lee por el 'Trust Server Certificate=True'
+
+            var conexion = @"Data Source =LAPTOP-GK1DJVHC\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
+            SqlDataAdapter adapter = new SqlDataAdapter(select, conexion);
+            adapter.Fill(dataTable);
+            gridNotipado.DataSource = dataTable;
+
+        }
+    }
+}
